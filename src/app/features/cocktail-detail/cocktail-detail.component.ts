4a8db75454acae5b916e6e394ceabd3bd5153cb4
spec18krish <spec18krish@gmail.com>
2024-07-06 12:56:42 +1200

    fixed few more issues
    fixed few more issues

--- src/app/features/cocktail-detail/cocktail-detail.component.ts
+++ src/app/features/cocktail-detail/cocktail-detail.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule, Location } from '@angular/common';
 import { Component, OnInit, inject, input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, map, take } from 'rxjs';
+import { Observable, catchError, map, of, take } from 'rxjs';
 import { Cocktail } from '../../core/models/coktail.interface';
 import { CocktailService } from '../../core/services/cocktail.service';
 import { FavoriteService } from '../../core/services/favorite.service';
@@ -15,16 +15,15 @@ import { IsAlcoholicDirective } from '../../shared/directives/is-alcoholic.direc
   templateUrl: './cocktail-detail.component.html'
 })
 export class CocktailDetailComponent implements OnInit {
-
-  cocktailId = input.required<number>();
   private _cocktailService = inject(CocktailService);
   private _activateRoute = inject(ActivatedRoute);
   private _favoritesService = inject(FavoriteService);
   private _locationService = inject(Location);
-  public cocktailDetail$!: Observable<Cocktail>;
+  public cocktailDetail$!: Observable<Cocktail | null>;
+  public errorMessage: string = '';
 
   ngOnInit(): void {
-    const cocktailId =  Number(this._activateRoute.snapshot.paramMap.get('id'));
+    const cocktailId: number =  Number(this._activateRoute.snapshot.paramMap.get('id'));
     this.loadCocktailDetails(cocktailId);
   }
 
@@ -32,23 +31,36 @@ export class CocktailDetailComponent implements OnInit {
     this._locationService.back();
   }
 
-  public onFavoritesClick(cocktailId: number) {
+  public onFavoritesClick(cocktailId: number): void {
     this._favoritesService.addOrRemoveFavorite(cocktailId);
     this.loadCocktailDetails(cocktailId);
   }
 
   private loadCocktailDetails(cocktailId: number): void {
-    this.cocktailDetail$ = this.getCockDetails(cocktailId);
+    if (cocktailId && !isNaN(cocktailId)) {
+      this.cocktailDetail$ = this.getCockDetails(cocktailId);
+    }
+    else {
+      this.errorMessage = 'Sorry No data found';
+    }
   }
 
-  private getCockDetails(cocktailId: number):  Observable<Cocktail>{
+  private getCockDetails(cocktailId: number): Observable<Cocktail | null>{
     const favoriteIds = this._favoritesService.getFavoriteIds();
     return this._cocktailService.getCocktailDetail(cocktailId).pipe(
       map(cocktail => {
+        if (!cocktail) {
+          this.errorMessage = "Sorry cocktail not found";
+          return null;
+        };
         return {
           ...cocktail,
-          isFavorite: favoriteIds.includes(+cocktail.id)
+          isFavorite: favoriteIds.includes(+cocktail?.id)
         }
+      }),
+      catchError(error => {
+        this.errorMessage = error;
+        return of(null)
       }),
       take(1)
     )
